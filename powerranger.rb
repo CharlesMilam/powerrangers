82f58555a4773e97bcae33af3f1b0b044c4b48ad
Milam <charles.milam@gmail.com>
2014-11-11 03:18:57 -0600

    Implement PowerRanger class. Modify Person class to collaborate with PowerRanger appropriately. Add test code.

--- powerranger.rb
+++ powerranger.rb
@@ -1,6 +1,9 @@
 # a person, who has a name and a caffeine level, and
 # can perform certain actions
 class Person
+	attr_reader :name
+	attr_accessor :caffeine_level
+
 	def initialize(name, caffeine = 0)
 		@name = name
 		@caffeine_level = caffeine
@@ -10,13 +13,13 @@ class Person
 	def run
 		case @caffeine_level
 		when 0..25
-			"#{@name} trots..."
+			"#{@name} trots away..."
 		when 26..50
-			"#{@name} runs..."
+			"#{@name} runs away..."
 		when 51..75
-			"#{@name} runs fast...gotta love coffee..."
+			"#{@name} runs away fast...gotta love coffee..."
 		when 75..100
-			"#{@name} runs really, really, fast...too much coffee, man..."
+			"#{@name} runs away really, really, fast...too much coffee, man..."
 		else
 			"#{@name} does nothing...either not enough coffee, or heart has exploded from too much..."
 		end
@@ -26,15 +29,15 @@ class Person
 	def scream
 		case @caffeine_level
 		when 0..25
-			"Eeek!..."
+			"#{@name} screams Eeek!..."
 		when 26..50
-			"Yelp!!..."
+			"#{@name} screams Yelp!!..."
 		when 51..75
-			"Yar!!!..."
+			"#{@name} screams Yar!!!..."
 		when 75..100
-			"Rawr!!!!1!!!11!!!eleven!!!..."
+			"#{@name} screams Rawr!!!!1!!!11!!!eleven!!!..."
 		else
-			"Um, errr, more coffee, please..."
+			"#{@name} says Um, errr, more coffee, please..."
 		end
 	end
 
@@ -47,7 +50,36 @@ class Person
 
 end
 
-class PowerRanger
+class PowerRanger < Person
+	def initialize(color, strength = 5)
+		super(@caffeine_level)
+		@color = color
+		@strength = strength
+	end
+
+	def punch(person)
+		action = []
+		@caffeine_level -= 10
+		unless @strength > 5
+			action << "The #{@color} Power Ranger punches #{person.name}"
+			action << "#{person.scream}. #{person.run}."
+			person.caffeine_level -= 20
+		else
+			action << "The #{@color} Power Ranger punches #{person.name}"
+			action << "The mighty punch sends #{person.name} somersaulting into the air"
+			action << "#{person.scream}. #{person.run}."
+			person.caffeine_level -= 30
+		end
+		action.join(". ")
+	end
+
+	def rest
+		"The #{@color} Power Ranger rests."
+	end
+
+	def use_megazord
+		"Uh, oh, looks like the #{@color} Power Ranger is using MEGAZORD, and you know what that means..."
+	end
 end
 
 class EvilNinja
@@ -57,18 +89,42 @@ person1 = Person.new("Charles", 33)
 puts person1.run
 puts person1.scream
 puts person1.drink_coffee
+puts "************************************\n"
 puts person1.run
 puts person1.scream
 puts person1.drink_coffee
+puts "************************************\n"
 puts person1.run
 puts person1.scream
 puts person1.drink_coffee
+puts "************************************\n"
 puts person1.run
 puts person1.scream
 puts person1.drink_coffee
+puts "************************************\n"
 puts person1.run
 puts person1.scream
 puts person1.drink_coffee
+puts "************************************\n"
 puts person1.run
 puts person1.scream
 puts person1.drink_coffee
+puts "************************************\n"
+puts person1.run
+puts person1.scream
+puts person1.drink_coffee
+puts "************************************\n"
+puts person1.run
+puts person1.scream
+puts "************************************\n"
+pr1 = PowerRanger.new("Pink", 10)
+puts pr1.punch(person1)
+puts "************************************\n"
+puts pr1.punch(evil_minion1 = Person.new("Evil Minion One"))
+puts "************************************\n"
+puts pr1.use_megazord
+puts "************************************\n"
+puts pr1.punch(person1)
+puts pr1.punch(person1)
+puts pr1.punch(person1)
+puts "************************************\n"
